995fec354ce1ec935d1c2439b8df6a5b8b8bd1b2
M.Randika <manularandika28@gmail.com>
2026-08-19 14:41:28 +0530

    maked some changes

--- interface/src/App.jsx
+++ interface/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import StudentDashboard from './pages/Student/StudentDashboard.jsx';
-import Demo from './pages/Student/Demo.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Demo from './pages/Student/Demo.jsx';
 
 function App() {
   return (
--- interface/vite.config.js
+++ interface/vite.config.js
@@ -1,2 +1,11 @@
 import { defineConfig } from 'vite';
-export default defineConfig({server:{port:5173}});
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+});
