15d081808186a654e862765b006eb378c4b51700
TharaniThiranja <tharanithiranjaraveendra246@gmail.com>
2026-08-27 01:27:22 +0530

    Add lecturer dashboards

--- interface/src/App.jsx
+++ interface/src/App.jsx
@@ -6,6 +6,11 @@ import HODDashboard from './pages/HOD/HOD_dashboard';
 import MeetingRequests from './pages/HOD/Meeting_requests';
 import MedicalReview from './pages/HOD/Medical_review';
 
+// Updated paths to include pages/Lecturer/
+import LecturerDashboard from './pages/Lecturer/LecturerDashboard';
+import AttendanceManager from "./pages/Lecturer/AttendanceManager";
+import MeetingManagement from "./pages/Lecturer/MeetingManagement";
+
 function App() {
   return (
     <BrowserRouter>
@@ -13,6 +18,9 @@ function App() {
         <Route path="/hod/dashboard" element={<HODDashboard />} />
         <Route path="/hod/meetings" element={<MeetingRequests />} />
         <Route path="/hod/medical" element={<MedicalReview />} />
+        <Route path="/lecturer/dashboard" element={<LecturerDashboard />}/>
+        <Route path="/lecturer/attendance" element={<AttendanceManager />}/>
+        <Route path="/lecturer/meetings" element={<MeetingManagement />}/>
         <Route path="*" element={<Navigate to="/hod/medical" replace />} />
       </Routes>
     </BrowserRouter>
