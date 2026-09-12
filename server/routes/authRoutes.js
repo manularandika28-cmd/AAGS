93c648bd87c30b25aca0ddb3e3cf7c9fbd94f178
rahuaraf813-create <rahuaraf813@gmail.com>
2026-09-12 19:03:10 +0530

    set register route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { login, refreshToken, logout } from '../controllers/authController.js';
+import { login, refreshToken, register, logout } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.get('/refresh', refreshToken);
+router.post('/register', register);
 router.post('/logout', logout);
 
 export default router;
