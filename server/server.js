f1633ffa1367a0ae1e912e0682fc8fa420378009
rr9155191-maker <rr9155191@gmail.com>
2026-09-23 19:56:03 +0530

    add dean server backend

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import { pool } from './db.js';
 import authRoutes from './routes/authRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
+import deanRoutes from './routes/deanRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -45,6 +46,7 @@ pool.connect((err, client, release) => {
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
+app.use('/api/dean', deanRoutes);
 
 // Start the server
 app.listen(port, '0.0.0.0', () => {
